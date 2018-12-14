ddee0df3ba4211ea8ba787ec982284ba55a47534
kentaromatsuura <1122805.km.1020@gmail.com>
2018-12-14 21:32:39 +0900

    統合

--- src/router.js
+++ src/router.js
@@ -53,5 +53,21 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/Toppage.vue')
     },
+    {
+      path: '/works',
+      name: 'works',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Works.vue')
+    },
+    {
+      path: '/calender',
+      name: 'calender',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Calender.vue')
+    }
   ]
 })
