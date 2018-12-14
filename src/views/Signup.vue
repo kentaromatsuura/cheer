1ffcf0a38761b1821a24724139b618fc061999fb
kentaromatsuura <1122805.km.1020@gmail.com>
2018-12-14 14:44:51 +0900

    firebaseでアカウント作製機能

---
+++ src/views/Signup.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-app>
+      <v-content>
+        <v-container>
+          <v-layout row>
+            <v-flex xs12>
+              <h3>SignUp</h3>
+              <v-text-field v-model="email" placeholder="email">
+              </v-text-field>
+              <v-text-field type="password" v-model="password" placeholder="password">
+              </v-text-field>
+              <v-btn v-on:click="signUp">SignUp</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </v-app>
+</template>
+<script>
+import firebase from "firebase";
+export default {
+  name: "Signup",
+  data() {
+      return {
+        email: null,
+        password: null
+      }
+  },
+  methods: {
+    signUp: function () {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .then(user => {
+          alert('Create account: ', this.email);
+          console.log(user);
+        })
+        .catch(error => {
+          alert(error.message);
+        });
+    }
+  }
+}
+</script>
