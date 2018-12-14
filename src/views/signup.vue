fd969784bbb90d9e50329adb6cb38d0bd46dcfa1
fem10 <fem1008drive@gmail.com>
2018-12-14 15:00:48 +0900

    テスト

---
+++ src/views/signup.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="signup">
+    <h1>Signup</h1>
+    <span class="input-group-addon">ユーザー名</span>
+	<input type="text" class="form-control" placeholder="名前を記入してください">
+  </div>
+</template>
